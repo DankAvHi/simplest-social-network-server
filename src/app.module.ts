e4a7fda47f55de80755c56d67ddfe0a6c06dc79d
DankAvHi <thexcat86@gmail.com>
2023-02-04 18:26:08 +0300

    [add] config, graphql, user, prisma modules to app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,41 @@
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import appConfig from "src/config/appConfig";
+import { GraphQLModule } from "@nestjs/graphql";
+import { join } from "path";
+import configuration from "src/config/configuration";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { PrismaService } from "./prisma/prisma.service";
+import { UsersModule } from "./users/users.module";
+import { AuthModule } from './auth/auth.module';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: join(
+        process.cwd(),
+        `.env.${process.env.NODE_ENV || "production"}`
+      ),
+      load: [configuration],
+    }),
+
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ["./**/*.graphql"],
+      definitions: {
+        path: join(process.cwd(), "src/graphql.ts"),
+        outputAs: "class",
+      },
+      playground: process.env.GRAPHQL_PLAYGROUND == "true",
+    }),
+
+    UsersModule,
+
     AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {
   constructor(appService: AppService) {
