d89dd0cffababe71a57e688778f42a727094df04
DankAvHi <thexcat86@gmail.com>
2023-02-04 18:27:18 +0300

    [db crt] created entity clases from gql schema

---
+++ src/graphql.ts
@@ -0,0 +1,61 @@
+
+/*
+ * -------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+
+export enum role {
+    admin = "admin",
+    moderator = "moderator",
+    user = "user"
+}
+
+export class CreateUserInput {
+    email: string;
+    first_name?: Nullable<string>;
+    last_name?: Nullable<string>;
+    patronymic?: Nullable<string>;
+    login: string;
+    gender?: Nullable<boolean>;
+    password: string;
+}
+
+export class UpdateUserInput {
+    first_name?: Nullable<string>;
+    last_name?: Nullable<string>;
+    patronymic?: Nullable<string>;
+    login?: Nullable<string>;
+    gender?: Nullable<boolean>;
+}
+
+export class User {
+    id: number;
+    email: string;
+    first_name?: Nullable<string>;
+    last_name?: Nullable<string>;
+    patronymic?: Nullable<string>;
+    login: string;
+    gender?: Nullable<boolean>;
+    role?: Nullable<role>;
+    password: string;
+}
+
+export abstract class IQuery {
+    abstract users(): Nullable<User>[] | Promise<Nullable<User>[]>;
+
+    abstract user(id: number): Nullable<User> | Promise<Nullable<User>>;
+}
+
+export abstract class IMutation {
+    abstract createUser(createUserInput: CreateUserInput): User | Promise<User>;
+
+    abstract updateUser(updateUserInput: UpdateUserInput): User | Promise<User>;
+
+    abstract removeUser(id: number): Nullable<User> | Promise<Nullable<User>>;
+}
+
+type Nullable<T> = T | null;
