f732087ae71e7fef8bc909f21530f8eaea3d1cef
DankAvHi <thexcat86@gmail.com>
2023-02-09 15:23:07 +0300

    [db] regenerate graphql types

--- src/graphql.ts
+++ src/graphql.ts
@@ -34,6 +34,7 @@ export class UpdateUserInput {
 }
 
 export class User {
+    __typename?: 'User';
     id: number;
     email: string;
     first_name?: Nullable<string>;
@@ -46,12 +47,16 @@ export class User {
 }
 
 export abstract class IQuery {
+    __typename?: 'IQuery';
+
     abstract users(): Nullable<User>[] | Promise<Nullable<User>[]>;
 
     abstract user(id: number): Nullable<User> | Promise<Nullable<User>>;
 }
 
 export abstract class IMutation {
+    __typename?: 'IMutation';
+
     abstract createUser(createUserInput: CreateUserInput): User | Promise<User>;
 
     abstract updateUser(updateUserInput: UpdateUserInput): User | Promise<User>;
